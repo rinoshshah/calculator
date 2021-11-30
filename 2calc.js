57c326b34c97061adc0aa36f6f9a05a086b92e60
Rinosh Shah <rinosh.shah@gmail.com>
2021-11-30 09:04:53 +0530

    initial Commit

---
+++ 2calc.js
@@ -0,0 +1,7 @@
+var displayValue=(num)=>result.value+=num
+
+var clearBox=()=>result.value=""
+
+var evaluateExpression=()=>result.value=eval(result.value)
+
+var backspace=()=>result.value=result.value.slice(0,-1)
